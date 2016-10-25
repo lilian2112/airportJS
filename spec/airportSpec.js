043eb699ff05e97825ee7b59cbccb549c52814db
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-25 16:15:57 +0100

    plane can take off

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -4,6 +4,7 @@ describe("Airport", function() {
   beforeEach(function(){
     airport = new Airport();
     plane = new Plane();
+    plane2 = new Plane();
   });
 
   it ("has the plane after it has landed", function (){
@@ -13,8 +14,15 @@ describe("Airport", function() {
 
   it ("raises an error when an already landed plane tries to land", function (){
     airport.land(plane);
-    // spyOn(plane, 'land');
     expect(function() {airport.land(plane)}).toThrow("Plane already landed");
+  });
+  it ("raises an error when airport is full and plane tries to land", function (){
+    airport.land(plane);
+    expect(function() {airport.land(plane2)}).toThrow("Airport is full");
+  });
+  it ("no longer has plane after taken off", function (){
+    airport.takeOff(plane);
+    expect(airport._planes).not.toContain(plane);
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -4,10 +4,14 @@ function Airport(){
 
 Airport.prototype.land = function (plane){
   if (plane.isLanded === true) {
-    console.log(plane.isLanded)
     throw ("Plane already landed");
+  } else if (this._planes.length >= 1) {
+    throw ("Airport is full");
   } else {
     plane.land();
     this._planes.push(plane)
   }
+}
+Airport.prototype.takeOff = function () {
+  this._planes.pop
 }
